3eb77a59e88a212b44338b8fcf54641c62eee3fe
FearlessIIX <fearlessiix563@gmail.com>
2021-11-12 13:47:35 -0500

    started on main functionality of the program

--- Main.py
+++ Main.py
@@ -1,8 +1,54 @@
 def main():
-    pass
+    employee_directory()
 
 
 def employee_directory():
+    # TODO implement the process of getting values from storage
+    employee_dir = {}
+    while True:
+        command = input(f"{print_menu()}\n")
+        # lookup
+        if command == "1":
+            lookup_employee()
+        # add
+        elif command == "2":
+            add_employee()
+        # change
+        elif command == "3":
+            pass
+        # delete
+        elif command == "4":
+            pass
+        # quit
+        elif command == "5":
+            pickle(employee_dir)
+
+
+def print_menu():
+    return ("Type the number for your desired action\n"
+            " 1) Look Up Employee. 2) Add New Employee. \n"
+            " 3) Change Existing Employees Information. 4) Delete An Employee\n"
+            " 5) Quit The Program")
+
+
+def lookup_employee():
+    pass
+
+
+def change_employee():
+    pass
+
+
+def delete_employee():
+    pass
+
+
+def add_employee():
+    pass
+
+
+# TODO implement the pickling process
+def pickle(employees):
     pass
 
 
@@ -19,8 +65,11 @@ class Employee:
         self.job_title = title
 
     def get_name(self): return self._name
+
     def get_id(self): return self._ID
+
     def get_department(self): return self._department
+
     def get_job_title(self): return self.job_title
 
     def change_name(self, new_name):
